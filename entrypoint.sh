f74895c616558c672211ff1a0f685065ecd549b1
Toukoms <tokiniaina090605@gmail.com>
2025-04-18 10:56:13 +0300

    chore: add action.yml file

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ git config --global user.email "${INPUT_EMAIL}"
 python3 /urs/bin/feed.py
 
 git add -A && git commit -m "Update feed"
-
 git push --set-upstream origin main
 
 echo "==========================="
