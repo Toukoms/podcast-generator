f638efdfae25a4547d42fa1e2aef36399efc7d2f
Toukoms <tokiniaina090605@gmail.com>
2025-04-18 11:50:17 +0300

    fix: correct path to feed.py in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "==========================="
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 
-python3 /urs/bin/feed.py
+python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update feed"
 git push --set-upstream origin main
